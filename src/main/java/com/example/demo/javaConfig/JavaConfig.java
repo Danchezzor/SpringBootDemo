5e5d482477c6d45c481fdbfb75b698aaf00ec7bf
danchezzor <daniilko95@gmail.com>
2024-10-31 12:21:47 +0300

    Доработка!

--- src/main/java/com/example/demo/javaConfig/JavaConfig.java
+++ src/main/java/com/example/demo/javaConfig/JavaConfig.java
@@ -3,17 +3,20 @@ package com.example.demo.javaConfig;
 import com.example.demo.profile.DevProfile;
 import com.example.demo.profile.ProductionProfile;
 import com.example.demo.profile.SystemProfile;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class JavaConfig {
     @Bean
+    @ConditionalOnProperty(value = "netology.profile.dev", havingValue = "true")
     public SystemProfile devProfile() {
         return new DevProfile();
     }
 
     @Bean
+    @ConditionalOnProperty(value = "netology.profile.dev", havingValue = "false")
     public SystemProfile prodProfile() {
         return new ProductionProfile();
     }
